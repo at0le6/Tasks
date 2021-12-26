45f23b46c305577a66a1bd6f6684727db34a81ce
Gamaliel <angel.gamaliel@hotmail.com>
2021-12-25 21:33:56 -0600

    End of proyect TASK APP

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(errorHandlerMiddleware);
 //app.patch('/api/v1/tasks/:id)  -update tasks
 //app.patch('/api/v1/tasks/:id)  -delete tasks
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async() => {
     try {
         await connectDB(process.env.MONGO_URI)
